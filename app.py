564c163b798f01d645c5605f78f433ba2385dc36
Nathan <nathan.acamolez@gmail.com>
2024-01-17 19:18:55 -0300

    added_product func

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    price = db.Column(db.Float, nullable=True)
+    description = db.Column(db.Text, nullable=False)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(
+            name=data['name'],
+            price=data['price'],
+            description=data.get('description', ''))
+
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
